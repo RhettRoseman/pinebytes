219bd9662303caca3a1f91988c364aaf05a091fd
Rhett Roseman <rroseman@rcplanners.com>
2025-11-10 15:11:35 -0500

    layout.tsx fix

--- pinebyte/app/layout.tsx
+++ pinebyte/app/layout.tsx
@@ -51,7 +51,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "./components/Navtabs";
+import Footer from "./components/footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -69,7 +70,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Navbar />
       <body className={inter.className}>{children}</body>
+    <Footer />
     </html>
   );
 }
