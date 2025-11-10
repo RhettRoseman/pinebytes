03c5a66ecb4f43e7e8fbc271a32bdff75ba4a206
Rhett Roseman <rroseman@rcplanners.com>
2025-11-10 11:43:19 -0500

    Suspense needs to be around a component for useSearchParams to work bug fix

--- pinebyte/app/packages/page.tsx
+++ pinebyte/app/packages/page.tsx
@@ -1,9 +1,30 @@
 import PackagesForm from '../components/packageform';
-
-export default function PackagePage(){
-    return(
+import { Suspense } from 'react';
+import Image from 'next/image';
+export default function PackagePage() {
+    return (
         <div>
-            <PackagesForm/>
+            <Suspense fallback={
+                <div className="min-h-screen flex items-center justify-center">
+                    <div>
+                        <Image
+                            src="/pinebytelogo.png"
+                            alt="Pinebyte Logo"
+                            width={150}
+                            height={150}
+                            className="animate-pulse"
+                        />
+                    </div>
+
+                    <p className="mt-4 text-green-900 font-semibold">
+                        Loading...
+                    </p>
+                </div>
+            }
+            >
+                <PackagesForm />
+            </Suspense>
+
         </div>
     )
 }
