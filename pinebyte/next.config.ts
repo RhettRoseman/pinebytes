ce227149fd8dc05a6d2179a4fd3c0875f5c1d108
Rhett Roseman <rroseman@rcplanners.com>
2026-01-19 17:36:14 -0500

    trailing slash bug fix

--- pinebyte/next.config.ts
+++ pinebyte/next.config.ts
@@ -1,11 +1,11 @@
 import type { NextConfig } from "next";
 
-
 const nextConfig: NextConfig = {
   output: 'export',
+  trailingSlash: false, // <-- add this line
   images: {
     unoptimized: true,
   },
-}
+};
 
 export default nextConfig;
